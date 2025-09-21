dbd38a9c8fea9d5446b98feb7115524f5a22b909
Mudasir Irshad <mudasirirshad47@gmail.com>
2025-09-21 10:00:00 +0500

    Readme for hello agent how things work and why we need them

--- hello_agent_class02/main.py
+++ hello_agent_class02/main.py
@@ -1,24 +1,19 @@
 from dotenv import load_dotenv
 import os
 
-from agents import Agent, Runner, OpenAIChatCompletionsModel, AsyncOpenAI, RunResult # type: ignore
-
+from agents import Agent, Runner, OpenAIChatCompletionsModel
+from openai import AsyncOpenAI
 
 load_dotenv()
 api_key = os.getenv('GOOGLE_API_KEY')
 
-external_client: AsyncOpenAI = AsyncOpenAI(
-    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
-    api_key=api_key
+client = AsyncOpenAI(
+    api_key= api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
 
-llm : OpenAIChatCompletionsModel = OpenAIChatCompletionsModel(
-    model="gemini-2.5-flash",
-    openai_client=external_client
-)
+agent: Agent = Agent(name="Assistant", instructions="You are a motivational assistant", model=OpenAIChatCompletionsModel(model="gemini-2.5-flash", openai_client=client))
 
-agent: Agent = Agent(name="Assistant", instructions="You are a motivational assistant", model=llm)
-
-result: RunResult = Runner.run_sync(agent, "why learn math for AI Agents?")
+result = Runner.run_sync(agent, "why learn math for AI Agents?")
 
 print(result.final_output)
