db0126a9ff86ddd8f894bd6e7b1f379459607d4d
Mudasir Irshad <mudasirirshad47@gmail.com>
2025-09-23 10:00:00 +0500

    Run stream event

--- Personal Learning/01_streaming_output/main.py
+++ Personal Learning/01_streaming_output/main.py
@@ -21,13 +21,11 @@ async def main():
     )
     agent: Agent = Agent(name="AI Assistant", instructions="You are an AI Teacher which give solutions to any question.", model=llm)
 
-    res = Runner.run_streamed(agent, "1000 word essay on pakistan")
+    res = Runner.run_streamed(agent, input("What you want to ask? "))
 
     async for event in res.stream_events():
         if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
-            print(event.data.delta, end="", flush=True)
-
-
+            print(event.data.delta, end="\n", flush=True)
 
 
 asyncio.run(main())
