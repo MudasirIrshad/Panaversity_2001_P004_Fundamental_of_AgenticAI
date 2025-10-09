d05f3eae2c0972567e8798a54d0dc2a581ed5108
Mudasir Irshad <mudasirirshad47@gmail.com>
2025-10-09 11:44:15 +0500

    Ai speech agent

--- Panaversity Class Learning/context_types/main.py
+++ Panaversity Class Learning/context_types/main.py
@@ -3,6 +3,11 @@ import os
 from dataclasses import dataclass
 from agents import Agent, Runner, OpenAIChatCompletionsModel, set_tracing_disabled, function_tool, RunContextWrapper
 from openai import AsyncOpenAI
+import speech_recognition as sr
+import pyttsx3
+
+
+r = sr.Recognizer()
 load_dotenv()
 
 
@@ -49,16 +54,69 @@ def get_top_islamic_personalities(local_context:RunContextWrapper, name: str)->s
     print(f"\n\n [CONTEXT] Getting data for {name}\n\n")
     return f"This is about {res}."
 
+@function_tool
+def speech_to_text(audio:str):
 
 
-agent: Agent = Agent(name="Assistant", instructions="You are my Assistant. Use the tool to find details about Islamic personalities. after getting response from the tool try to give output in this way \n[RESULT] <Result from tool> \n[EXPLANATION] <Try to enhance the tool response and give a explained output here >",model=llm, tools=[get_top_islamic_personalities])
+    print("\n\nCalling speech to text tool now\n\n")
+    with sr.AudioFile(audio) as source:
+        audio_data = r.record(source)
+        
+    try:
+        text = r.recognize_google(audio_data) # type: ignore
+        print("Transcribed Text:", text)
+    except sr.UnknownValueError:
+        print("Could not understand audio")
+    except sr.RequestError:
+        print("Could not request results from Google Speech Recognition service")
+    return f"{text}"
+
+@function_tool
+def text_to_speech(text: str):
+    """Convert any text to speech using system voice"""
+    print("\n\n[TOOL] Speaking the LLM output...\n\n")
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 175)   # speed of voice
+    engine.setProperty('volume', 0.9) # volume level (0.0 to 1.0)
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)  # type: ignore # choose voice
+    engine.say(text)
+    engine.runAndWait()
+    engine.stop()
+    return "Spoken successfully."
+
+
+agent: Agent = Agent(name="Assistant", instructions="""
+You are my intelligent Assistant.
+Your purpose is to provide accurate and meaningful information about famous Islamic personalities.
+
+Behavior Rules:
+
+If the user provides an audio file, first use the speech_to_text tool to convert the speech into text.
+
+Once you have the transcribed text, or if the user’s input is already in text form, call the get_top_islamic_personalities tool to find detailed information related to the query.
+
+After receiving the response from the tool, present your final answer in the following format:
+
+[RESULT] <Provide the direct information or key findings here.>
+
+[EXPLANATION] <Give a clear, well-written explanation that expands on the result — include context, history, and any relevant insights to make it educational and engaging.>
+
+
+After showing your response, automatically call the text_to_speech tool to convert your full answer into spoken audio so it can be listened to easily.
+
+Tone:
+Stay respectful, clear, and informative — like a calm, helpful teacher explaining to a student. Avoid unnecessary details and keep responses natural and human-like.
+""",
+model=llm, 
+tools=[speech_to_text,get_top_islamic_personalities, text_to_speech])
 
 
 data_context = Top_Islamic_Personalities("data.txt")
 
 result = Runner.run_sync(
     starting_agent=agent, 
-    input="Tell me about umar",
+    input="audio.wav",
     context=data_context
     )
 
