9550abd62e59d11d35caa1ca553bd31f5bd09735
Mudasir Irshad <mudasirirshad47@gmail.com>
2025-10-15 17:08:23 +0500

    set tracing

--- Panaversity Class Learning/08_Observability_and_Agent_clonning/main.py
+++ Panaversity Class Learning/08_Observability_and_Agent_clonning/main.py
@@ -1,14 +1,14 @@
 from dotenv import load_dotenv
 import os
 
-from agents import Agent, RunConfig, RunResult, Runner, OpenAIChatCompletionsModel
+from agents import Agent, RunConfig, RunContextWrapper, RunResult, Runner, OpenAIChatCompletionsModel, function_tool, trace
 from openai import AsyncOpenAI
 
 
 # Load environment variables
 load_dotenv()
 api_key = os.getenv('GOOGLE_API_KEY')
-print(api_key)
+
 client = AsyncOpenAI(
     api_key=api_key,
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
@@ -21,13 +21,36 @@ llm: OpenAIChatCompletionsModel = OpenAIChatCompletionsModel(
 )
 
 
+@function_tool
+def add_number(a:int, b:int)->int:
+    """Show all the steps needed to add 2 numbers"""
+    return a+b
+@function_tool
+def sub_number(a:int, b:int)->int:
+    """Show all the steps needed to subtract 2 numbers"""
+    return a-b
+
+def get_instructions(context: RunContextWrapper, agent: Agent):
+    return "Hi there, you are a Math assistant to solve basic questions using only tools."
 # Define agents
-math_agent = Agent(name="Assistant", instructions="You are a Math assistant", model=llm)
+agent = Agent(
+    name="My Assistant", 
+    instructions=get_instructions, 
+    model=llm, tools=[add_number,sub_number])
 
+agent.clone()
 
-result: RunResult = Runner.run_sync(
-    math_agent,
-    input="Why learn math for AI agents?",    
-)
+with trace("Math Trace"):
+    result: RunResult = Runner.run_sync(
+        agent,
+        input="what is the answer if we add 9 and 5, what is the answer if we 9 minus 5, both with steps?",
+    )
 
-print(result.final_output)
+    print(result.final_output)
+
+    # result: RunResult = Runner.run_sync(
+    #     math_agent,
+    #     input="what is the answer if we subtract 9 and 5 with steps?",
+    # )
+
+    # print(result.final_output)
