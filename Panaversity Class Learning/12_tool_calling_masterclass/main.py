a25bc23de247b933af8ef568355085ed3ccd1933
Mudasir Irshad <mudasirirshad47@gmail.com>
2025-11-01 10:00:00 +0500

    tool mastering

--- Panaversity Class Learning/12_tool_calling_masterclass/main.py
+++ Panaversity Class Learning/12_tool_calling_masterclass/main.py
@@ -1,7 +1,8 @@
+from dataclasses import dataclass
 from dotenv import load_dotenv
 import os
 
-from agents import Agent, RunResult, Runner, OpenAIChatCompletionsModel 
+from agents import Agent, RunContextWrapper, RunResult, Runner, OpenAIChatCompletionsModel, StopAtTools , function_tool
 from openai import AsyncOpenAI
 
 
@@ -21,16 +22,75 @@ llm : OpenAIChatCompletionsModel = OpenAIChatCompletionsModel(
     model="gemini-2.5-flash",
     openai_client=client
 )
-# Define agents
-agent = Agent(name="Assistant", instructions="You are a Math assistant", model=llm)
 
 
-# Run a query
-result: RunResult = Runner.run_sync(
+# @function_tool
+# def add_numbers(a, b):
+#     ""
+#     print("Add two number tool called")
+#     return a+b
+# @function_tool
+# def sub_numbers(a, b):
+#     ""
+#     print("Subtract Tool")
+#     return a-b
+# # Define agents
+
+
+
+
+
+# agent = Agent(
+#     name="Assistant",
+#     instructions="You are a Math assistant, which answer math queries and give answer with a joke always",
+#     tools=[add_numbers, sub_numbers], 
+#     model=llm,
+#     tool_use_behavior="stop_on_first_tool" #Will call tools but give only first tool answer.
+#     # tool_use_behavior=StopAtTools(stop_at_tool_names=["sub_numbers"]) #Stops on specific tool
+#     )
+
+
+# # Run a query
+# result: RunResult = Runner.run_sync(
     
-    starting_agent=agent,
-    input="Why learn math for AI agents?",
+#     starting_agent=agent,
+#     input="sum 1 from 5, sub 5 and 29",
+#     max_turns=3
     
+# )
+
+# print(result.final_output)
+
+
+# Example 2
+@dataclass
+class user_info:
+    is_study_session: bool
+
+
+def check_boolean(user_context: RunContextWrapper[user_info], agent: Agent)->bool: 
+    return not user_context.context.is_study_session
+
+
+@function_tool(is_enabled=check_boolean) # type: ignore
+def search_google(query: str)->str:
+    print("Google Search TOOL")
+    return f"Searching google for query: {query}"
+
+# is_enabled: bool | ((RunContextWrapper[Any], AgentBase[Any]) -> MaybeAwaitable[bool]) = True
+
+agent: Agent = Agent(
+    name="Study Agent",
+    model=llm,
+    tools=[search_google]
 )
 
-print(result.final_output)
+user_context = user_info(is_study_session=False)
+
+runner = Runner.run_sync(
+    starting_agent=agent,
+    input="Search google for Who is Obama?",
+    context=user_context
+)
+
+print(runner.final_output)
